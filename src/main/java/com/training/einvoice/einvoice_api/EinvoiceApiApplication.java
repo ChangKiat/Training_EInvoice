a52871ed35a32fefe82728d78beb7a7ad97ae2a3
ChangKiat <changkiat1995@gmail.com>
2025-12-02 22:30:31 +0800

    SpringBoot Project

---
+++ src/main/java/com/training/einvoice/einvoice_api/EinvoiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.training.einvoice.einvoice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinvoiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinvoiceApiApplication.class, args);
+	}
+
+}
