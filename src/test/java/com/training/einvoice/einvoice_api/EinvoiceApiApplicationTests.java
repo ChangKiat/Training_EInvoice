f31051ecb969d8801b92bc4b013988ca1fe589fa
ChangKiat <changkiat1995@gmail.com>
2025-12-02 22:30:31 +0800

    SpringBoot Project

---
+++ src/test/java/com/training/einvoice/einvoice_api/EinvoiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.einvoice.einvoice_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinvoiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
