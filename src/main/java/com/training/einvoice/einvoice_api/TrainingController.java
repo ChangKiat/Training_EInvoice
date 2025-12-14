2fe5805db7677ca7e323aa9dfafb49fa09e08bd4
ChangKiat <changkiat1995@gmail.com>
2025-12-14 23:38:44 +0800

    Add log4j

--- src/main/java/com/training/einvoice/einvoice_api/TrainingController.java
+++ src/main/java/com/training/einvoice/einvoice_api/TrainingController.java
@@ -1,13 +1,17 @@
 package com.training.einvoice.einvoice_api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrainingController {
-
+	private static final Logger log =
+	        LoggerFactory.getLogger(TrainingController.class);
 	@RequestMapping("/")
 	public String greet() {
+		log.info("Hello World");
 		return "Hello World";
 	}
 }
